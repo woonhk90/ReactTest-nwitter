03cd7e8994485a1f4e337b85750da5e8a95414b4
woonhk90 <wonho7344@gmail.com>
2023-03-21 10:05:18 +0900

    firebase 초기세팅

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4o0u-iR8JYoQ-svhZ5BhplG06sbOru_A",
+  authDomain: "nwitter-edfad.firebaseapp.com",
+  projectId: "nwitter-edfad",
+  storageBucket: "nwitter-edfad.appspot.com",
+  messagingSenderId: "948230104966",
+  appId: "1:948230104966:web:53b4cc6306ff3547e0ee07",
+};
+
+export default firebase.initializeApp(firebaseConfig);
